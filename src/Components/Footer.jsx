cedf1f1d0c29c28267af1d0705c43275927188f4
Fiorella Laveglia <fiolaveglia@gmail.com>
2024-06-20 20:02:46 -0300

    Social Icons Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebookF, faTiktok, faWhatsapp  } from '@fortawesome/free-brands-svg-icons';
+
 
 const Footer = () => {
   return (
     <footer>
+      <div className="foot-init">
         <p>Powered by</p>
         <img src='/images/DH.png' alt='DH-logo' />
+      </div>
+        <ul className='social-list'>
+          <li><FontAwesomeIcon icon={faInstagram} style={{fontSize: '24px'}}/></li>
+          <li><FontAwesomeIcon icon={faWhatsapp} style={{ fontSize: '24px' }} /></li>
+          <li> <FontAwesomeIcon icon={faFacebookF} style={{ fontSize: '24px' }}/></li>
+          <li><FontAwesomeIcon icon={faTiktok} style={{ fontSize: '24px' }} /></li>
+        </ul>
     </footer>
   )
 }
